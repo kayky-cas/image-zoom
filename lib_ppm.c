4f52e4909f5f8c3e24b15996ce6cb5791ac9d3ed
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2023-04-20 19:18:33 -0300

    Logica errada

--- lib_ppm.c
+++ lib_ppm.c
@@ -12,106 +12,106 @@
 
 int read_ppm(char *file, struct image_s *image)
 {
-	FILE *fptr;
-	char *t;
-	char buf[128];
-	int r, d, i, j;
-	struct pixel_s *img_data;
-	
-	fptr = fopen(file, "rb");
-	if (!fptr)
-		return -1;
-		
-	t = fgets(buf, sizeof(buf), fptr);
-	if (!t)
-		goto failread;
-	
-	if (strncmp(buf, "P6\n", 3) != 0)
-		goto failread;
+    FILE *fptr;
+    char *t;
+    char buf[128];
+    int r, d, i, j;
+    struct pixel_s *img_data;
 
-        do {
-		t = fgets(buf, sizeof(buf), fptr);
-		if (!t)
-			goto failread;
-				
-	} while (strncmp(buf, "#", 1) == 0);
-	
-        r = sscanf(buf, "%u %u", &image->width, &image->height);
-        if (r < 2)
-		goto failread;
+    fptr = fopen(file, "rb");
+    if (!fptr)
+        return -1;
 
-        r = fscanf(fptr, "%u", &d);
-        if ((r < 1) || (d != 255))
-		goto failread;
-		
-        fseek(fptr, 1, SEEK_CUR);
-        
-        img_data = malloc(image->width * image->height * sizeof(struct pixel_s));
-        if (!img_data)
-		goto failread;
-        
-        image->pix = img_data;
-        
-        for (j = 0; j < image->height; j++)
-		for (i = 0; i < image->width; i++)
-			fread(&img_data[j * image->width + i], 3, 1, fptr);
-	
-	fclose(fptr);
+    t = fgets(buf, sizeof(buf), fptr);
+    if (!t)
+        goto failread;
 
-	return 0;
-	
+    if (strncmp(buf, "P6\n", 3) != 0)
+        goto failread;
+
+    do {
+        t = fgets(buf, sizeof(buf), fptr);
+        if (!t)
+            goto failread;
+
+    } while (strncmp(buf, "#", 1) == 0);
+
+    r = sscanf(buf, "%u %u", &image->width, &image->height);
+    if (r < 2)
+        goto failread;
+
+    r = fscanf(fptr, "%u", &d);
+    if ((r < 1) || (d != 255))
+        goto failread;
+
+    fseek(fptr, 1, SEEK_CUR);
+
+    img_data = malloc(image->width * image->height * sizeof(struct pixel_s));
+    if (!img_data)
+        goto failread;
+
+    image->pix = img_data;
+
+    for (j = 0; j < image->height; j++)
+        for (i = 0; i < image->width; i++)
+            fread(&img_data[j * image->width + i], 3, 1, fptr);
+
+    fclose(fptr);
+
+    return 0;
+
 failread:
-	fclose(fptr);
-	
-	return -1;
+    fclose(fptr);
+
+    return -1;
 }
 
 
 int write_ppm(char *file, struct image_s *image)
 {
-	FILE *fptr;
-	int i, j;
-	
-	fptr = fopen(file, "wb");
-	if (!fptr)
-		return -1;
-		
-	fprintf(fptr, "P6\n%d %d\n255\n", image->width, image->height);
-        for (j = 0; j < image->height; j++)
-		for (i = 0; i < image->width; i++)
-			fwrite(&image->pix[j * image->width + i], 3, 1, fptr);
+    FILE *fptr;
+    int i, j;
 
-	fclose(fptr);
-	
-	return 0;
+    fptr = fopen(file, "wb");
+    if (!fptr)
+        return -1;
+
+    fprintf(fptr, "P6\n%d %d\n255\n", image->width, image->height);
+    for (j = 0; j < image->height; j++)
+        for (i = 0; i < image->width; i++)
+            fwrite(&image->pix[j * image->width + i], 3, 1, fptr);
+
+    fclose(fptr);
+
+    return 0;
 }
 
 
 int new_ppm(struct image_s *image, int width, int height)
 {
-	struct pixel_s *img_data;
+    struct pixel_s *img_data;
 
-	image->width = width;
-	image->height = height;
-	
-        img_data = malloc(image->width * image->height * sizeof(struct pixel_s));
-        if (!img_data)
-		return -1;
-        
-        image->pix = img_data;
-        memset(image->pix, 0, image->width * image->height * sizeof(struct pixel_s));
-	
-	return 0;
+    image->width = width;
+    image->height = height;
+
+    img_data = malloc(image->width * image->height * sizeof(struct pixel_s));
+    if (!img_data)
+        return -1;
+
+    image->pix = img_data;
+    memset(image->pix, 0, image->width * image->height * sizeof(struct pixel_s));
+
+    return 0;
 }
 
 
 int free_ppm(struct image_s *image)
 {
-	if (image->pix) {
-		free(image->pix);
-		
-		return 0;
-	}
-	
-	return -1;
+    if (image->pix) {
+        free(image->pix);
+
+        return 0;
+    }
+
+    return -1;
 }
--- main.c
+++ main.c
@@ -1,7 +1,54 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
 #include "lib_ppm.h"
+
+void draw_pixels(struct image_s *zoom, uint8_t color, int color_pos, int width, int pix_pos) {
+    if (color >= 75 && color <= 134) {
+        if (color_pos == 0) {
+            zoom->pix[3 * width + pix_pos + color_pos].r = color;
+        } else if (color_pos == 1) {
+            zoom->pix[3 * width + pix_pos + color_pos].g = color;
+        } else {
+            zoom->pix[3 * width + pix_pos + color_pos].b = color;
+        }
+    } else if (color >= 135 && color <= 179) {
+        if (color_pos == 0) {
+            zoom->pix[0 * 3 * width + pix_pos + color_pos].r = color;
+            zoom->pix[2 * 3 * width + pix_pos + color_pos].r = color;
+        } else if (color_pos == 1) {
+            zoom->pix[0 * 3 * width + pix_pos + color_pos].g = color;
+            zoom->pix[2 * 3 * width + pix_pos + color_pos].g = color;
+        } else {
+            zoom->pix[0 * 3 * width + pix_pos + color_pos].b = color;
+            zoom->pix[2 * 3 * width + pix_pos + color_pos].b = color;
+        }
+    } else if (color >= 180) {
+        if (color_pos == 0) {
+            zoom->pix[0 * 3 * width + pix_pos + color_pos].r = color;
+            zoom->pix[1 * 3 * width + pix_pos + color_pos].r = color;
+            zoom->pix[2 * 3 * width + pix_pos + color_pos].r = color;
+        } else if (color_pos == 1) {
+            zoom->pix[0 * 3 * width + pix_pos + color_pos].g = color;
+            zoom->pix[1 * 3 * width + pix_pos + color_pos].g = color;
+            zoom->pix[2 * 3 * width + pix_pos + color_pos].g = color;
+        } else {
+            zoom->pix[0 * 3 * width + pix_pos + color_pos].b = color;
+            zoom->pix[1 * 3 * width + pix_pos + color_pos].b = color;
+            zoom->pix[2 * 3 * width + pix_pos + color_pos].b = color;
+        }
+    }
+}
+
+void pucrs(struct image_s *image, struct image_s *zoom, int width, int pix_pos) {
+    uint8_t r = image->pix[pix_pos].r;
+    draw_pixels(zoom, r, 0, width, pix_pos);
+    uint8_t g = image->pix[pix_pos].g;
+    draw_pixels(zoom, g, 1, width, pix_pos);
+    uint8_t b = image->pix[pix_pos].b;
+    draw_pixels(zoom, b, 2, width, pix_pos);
+}
 
 int main(int argc, char** argv) {
     assert(argc > 1);
@@ -18,13 +65,20 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    image->pix[0].r = 255;
-    image->pix[0].g = 255;
-    image->pix[0].b = 255;
-
-    write_ppm("nossa_imagem.ppm", image);
-
     free_ppm(image);
+
+    struct image_s data_zoom;
+    struct image_s *zoom = &data_zoom;
+
+    r = new_ppm(zoom, image->width * 3, image->height * 3);
+
+    for (int i = 0; i < image->width * image->height; i++) {
+        pucrs(image, zoom, image->width, i);
+    }
+
+    write_ppm("pucrs.ppm", zoom);
+
+    free_ppm(zoom);
 
     return 0;
 }
