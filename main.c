c00bd6b364e89c701c1b628bc00b67801bd3b12f
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2023-04-20 19:55:08 -0300

    removing if e else

--- main.c
+++ main.c
@@ -4,50 +4,26 @@
 #include <stdlib.h>
 #include "lib_ppm.h"
 
-void draw_pixels(struct image_s *zoom, uint8_t color, int color_pos, int width, int pix_pos) {
+void draw_pixels(struct image_s *zoom, uint8_t color, int color_pos, int width, int height, int pix_pos) {
     if (color >= 75 && color <= 134) {
-        if (color_pos == 0) {
-            zoom->pix[3 * width + (3 * pix_pos) + color_pos].r = color;
-        } else if (color_pos == 1) {
-            zoom->pix[3 * width + (3 * pix_pos) + color_pos].g = color;
-        } else {
-            zoom->pix[3 * width + (3 * pix_pos) + color_pos].b = color;
-        }
+        *((&zoom->pix[3 * width + (3 * pix_pos) + color_pos].r) + color_pos) = color;
     } else if (color >= 135 && color <= 179) {
-        if (color_pos == 0) {
-            zoom->pix[0 * 3 * width + (3 * pix_pos) + color_pos].r = color;
-            zoom->pix[2 * 3 * width + (3 * pix_pos) + color_pos].r = color;
-        } else if (color_pos == 1) {
-            zoom->pix[0 * 3 * width + (3 * pix_pos) + color_pos].g = color;
-            zoom->pix[2 * 3 * width + (3 * pix_pos) + color_pos].g = color;
-        } else {
-            zoom->pix[0 * 3 * width + (3 * pix_pos) + color_pos].b = color;
-            zoom->pix[2 * 3 * width + (3 * pix_pos) + color_pos].b = color;
-        }
+        *(&zoom->pix[0 * 3 * width + (3 * pix_pos) + color_pos].r + color_pos)  = color;
+        *(&zoom->pix[2 * 3 * width + (3 * pix_pos) + color_pos].r + color_pos) = color;
     } else if (color >= 180) {
-        if (color_pos == 0) {
-            zoom->pix[0 * 3 * width + (3 * pix_pos) + color_pos].r = color;
-            zoom->pix[1 * 3 * width + (3 * pix_pos) + color_pos].r = color;
-            zoom->pix[2 * 3 * width + (3 * pix_pos) + color_pos].r = color;
-        } else if (color_pos == 1) {
-            zoom->pix[0 * 3 * width + (3 * pix_pos) + color_pos].g = color;
-            zoom->pix[1 * 3 * width + (3 * pix_pos) + color_pos].g = color;
-            zoom->pix[2 * 3 * width + (3 * pix_pos) + color_pos].g = color;
-        } else {
-            zoom->pix[0 * 3 * width + (3 * pix_pos) + color_pos].b = color;
-            zoom->pix[1 * 3 * width + (3 * pix_pos) + color_pos].b = color;
-            zoom->pix[2 * 3 * width + (3 * pix_pos) + color_pos].b = color;
-        }
+        *(&zoom->pix[0 * 3 * width + (3 * pix_pos) + color_pos].r + color_pos) = color;
+        *(&zoom->pix[1 * 3 * width + (3 * pix_pos) + color_pos].r + color_pos) = color;
+        *(&zoom->pix[2 * 3 * width + (3 * pix_pos) + color_pos].r + color_pos) = color;
     }
 }
 
-void pucrs(struct image_s *image, struct image_s *zoom, int width, int pix_pos) {
+void pucrs(struct image_s *image, struct image_s *zoom, int pix_pos) {
     uint8_t r = image->pix[pix_pos].r;
-    draw_pixels(zoom, r, 0, width, pix_pos);
+    draw_pixels(zoom, r, 0, image->width, image->height, pix_pos);
     uint8_t g = image->pix[pix_pos].g;
-    draw_pixels(zoom, g, 1, width, pix_pos);
+    draw_pixels(zoom, g, 1, image->width, image->height, pix_pos);
     uint8_t b = image->pix[pix_pos].b;
-    draw_pixels(zoom, b, 2, width, pix_pos);
+    draw_pixels(zoom, b, 2, image->width, image->height, pix_pos);
 }
 
 int main(int argc, char** argv) {
@@ -73,7 +49,7 @@ int main(int argc, char** argv) {
     r = new_ppm(zoom, image->width * 3, image->height * 3);
 
     for (int i = 0; i < image->width * image->height; i++) {
-        pucrs(image, zoom, image->width, i);
+        pucrs(image, zoom, i);
     }
 
     write_ppm("pucrs.ppm", zoom);
